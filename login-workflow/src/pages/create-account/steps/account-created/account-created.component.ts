955e88ee71f87279d0d8145f1b120daecf2e2f2c
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-12-23 17:28:30 -0500

    Make account details customizable

--- login-workflow/src/pages/create-account/steps/account-created/account-created.component.ts
+++ login-workflow/src/pages/create-account/steps/account-created/account-created.component.ts
@@ -1,9 +1,15 @@
-import { Component, Input } from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {isEmptyView} from "../../../../util/view-utils";
 
 @Component({
     selector: 'pxb-create-account-account-created-step',
     template: `
-        <div class="mat-title pxb-auth-title">Account Created</div>
+        <div class="mat-title pxb-auth-title">
+            <ng-container *ngIf="isEmpty(accountCreatedTitleEl)">
+                {{ accountCreatedTitle }}
+            </ng-container>
+            <div #accountCreatedTitleVC><ng-content select="[pxb-account-created-title]"></ng-content></div>
+        </div>
         <div class="pxb-auth-full-height" style="justify-content: center;">
             <pxb-empty-state
                 class="pxb-account-created-empty-state"
@@ -16,9 +22,20 @@ import { Component, Input } from '@angular/core';
     `,
     styleUrls: ['./account-created.component.scss'],
 })
-export class PxbAccountCreatedComponent {
+export class PxbAccountCreatedComponent implements OnInit {
     @Input() email;
     @Input() userName;
+    @Input() accountCreatedTitle;
+
+    @ViewChild('accountCreatedTitleVC') accountCreatedTitleEl;
+
+    isEmpty = (el: ElementRef): boolean => isEmptyView(el);
+
+    ngOnInit(): void {
+        if (this.accountCreatedTitle === undefined) {
+            this.accountCreatedTitle = 'Account Created';
+        }
+    }
 
     getSuccessEmptyStateTitle(): string {
         if (this.userName && this.userName.trim()) {
